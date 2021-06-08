140f8a46d30abe2278ef919fc1ad6330f7c4abb3
Rokas <rookas.rudzenskas@gmail.com>
2021-06-08 15:17:30 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 import {useSelector} from "react-redux";
 import {selectUser} from "./features/userSlice";
 import Login from "./components/Login";
+import {auth} from "./firebase";
 
 function App() {
 
     const user = useSelector(selectUser);
+
+    useEffect(() => {
+        auth.onAuthStateChanged((authUser) => {
+            if(authUser) {
+                // the user is logged in
+            } else {
+                // not logged in
+
+            }
+        })
+    }, []);
 
   return (
     <div className="app">
