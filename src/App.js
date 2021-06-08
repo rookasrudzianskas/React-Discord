6320bc5f07ebc644e64a2951b766f788d53d09b3
Rokas <rookas.rudzenskas@gmail.com>
2021-06-08 08:42:51 +0300

    Roaks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
         <h1>Discord 🕶️</h1>
+        <Sidebar />
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAuq9XAihTVCW49q8YSOCMN-y0aVEBEYvQ",
+    authDomain: "rookas-discord-app.firebaseapp.com",
+    projectId: "rookas-discord-app",
+    storageBucket: "rookas-discord-app.appspot.com",
+    messagingSenderId: "22765471471",
+    appId: "1:22765471471:web:23bc078fff07ed771a7d1a",
+    measurementId: "G-EHS38D7SN0"
+};
