3f66c8a94786bc4d275a552127269dfe96401a00
Rokas <rookas.rudzenskas@gmail.com>
2021-06-08 15:25:25 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React, {useEffect} from 'react';
 import './App.css';
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
-import {useSelector} from "react-redux";
-import {selectUser} from "./features/userSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {login, logout, selectUser} from "./features/userSlice";
 import Login from "./components/Login";
 import {auth} from "./firebase";
 
 function App() {
 
     const user = useSelector(selectUser);
-
+    const dispatch = useDispatch();
     useEffect(() => {
         auth.onAuthStateChanged((authUser) => {
-            if(authUser) {
-                // the user is logged in
+            if (authUser) {
+                dispatch(
+                    login({
+                        uid: authUser.uid,
+                        photo: authUser.photoURL,
+                        email: authUser.email,
+                        displayName: authUser.displayName,
+                    })
+                );
             } else {
-                // not logged in
-
+                dispatch(logout());
             }
-        })
-    }, []);
+        });
+    }, [dispatch]);
 
   return (
     <div className="app">
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,24 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {
     user: null,
+    loading: true,
   },
-
   reducers: {
     login: (state, action) => {
-      state.user += action.payload;
+      state.user = action.payload;
+      state.loading = false;
     },
 
-    logout: (state) => {
+    logout: (state, action) => {
       state.user = null;
-    }
-  }
+      state.loading = false;
+    },
+  },
 });
 
 export const { login, logout } = userSlice.actions;
 
-export const selectUser = (state) => state.user.value;
+export const selectUser = (state) => state.user.user;
 
 export default userSlice.reducer;
