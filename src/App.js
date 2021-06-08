0e1233ffea87322d62d7cf38e67015ddb7d551d5
Rokas <rookas.rudzenskas@gmail.com>
2021-06-08 09:20:41 +0300

    Roaks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div className="app">
-        <h1>Discord 🕶️</h1>
         <Sidebar />
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import "./styles/Sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import CallIcon from "@material-ui/icons/Call";
+import MicIcon from "@material-ui/icons/Mic";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
+import { Avatar } from "@material-ui/core";
+import SidebarChannel from "./SidebarChannel";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            
+            <div className="sidebar__top">
+                <h3>Rokas Rudzianskas</h3>
+                <ExpandMoreIcon />
+            </div>
+
+            <div className="sidebar__channels">
+                <div className="sidebar__channelsHeader">
+                    <div className="sidebar__header">
+                        <ExpandMoreIcon />
+                        <h4>Text Channels</h4>
+                    </div>
+
+                    <AddIcon className="sidebar__addChannel" />
+                </div>
+                <div className="sidebar__channelsList">
+                    <SidebarChannel />
+                </div>
+            </div>
+
         </div>
     );
 };
